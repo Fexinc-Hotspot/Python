b2c26c89f79f634512fddd40a7b55b4c9d166ce4
innocent <geesnell.nich45@gmail.com>
2023-03-04 21:31:04 +0300

    first commit

--- variables.py
+++ variables.py
@@ -1,5 +1,126 @@
-#Example of Variables
+#Data Types we use type()function 
+#to know which class a variable or value belong
 
-my_string="Hello Python!"
+#num1=5
+#print(num1,'is a type',type(num1))
 
-print (my_string)
+#num2=2.5
+#print(num2,'is a type of',type(num2))
+
+#num4=1+4j
+#print(num4,'is a type of',type(num4))
+
+#List of Datatype
+
+#languages=["swift","java","python"]#we have created a list name languages with three string value inside it
+
+#we use index to acces the element
+
+#print(languages[2])
+#print(languages[1])
+#print(languages[0])
+
+#Tuple of Data type
+#tuple is the an ordered sequence of item same as list immutable
+
+#product=('Microsoft','xbox',499.90)
+
+#Access the elemnt through index
+#print(product[0])
+#print(product[2])
+
+#Datatype of String
+
+#name='python lovers'
+#print(name)
+#message='python for begginers'
+#print(message)
+
+#Set Datatype
+
+#student_id={121,12,422,456}
+#print(student_id)
+
+#Dictionary Datatype
+
+#capital_city={'nepal':'kathmandu','italy':'Rome','Tanzania':'Dar-es-salaam'}
+#We can retrieve value through keys
+#print(capital_city['nepal'])
+
+
+#Conversion of Data type
+#num1=5
+#num2=7.90
+
+#new_numx=num1+num2
+#print('The value of new no is',new_numx)
+
+#num_string='12'
+#num_integer=56
+
+#num_string=int(num_string)
+
+#num_sum= num_integer + num_string
+#print('The value of num_sum is :',num_sum)
+
+#Read the value from the keyboard and printout
+
+#number=int(input('Enter the value of x :'))
+#if number < 65: {
+#       # print('You still young :',number,'Years Old')
+#}
+#else:{
+  #      print('Your are Old & still Beautiful/Handsome :',number,'Years Old')
+        
+ #       }
+#name=input('Enter your name..:')
+#age=input('Enter your age...:')
+#location=input('Enter your location...:')
+#print('hello',name,'yours',age,'years old','your location is at',location)
+
+#num=input('Enter the values :')
+
+ 
+
+
+
+#my_list=input("Enter the values :")
+#print('\n')
+#user_list=my_list.split()
+#print('List :',user_list)
+
+#Convert each item into int type
+#for i in user_list:
+    
+    #user_list[i]=int(user_list[i])
+    #sum +=([i])
+    #print('\n')
+    
+#input_string = input("Enter a list element separated by space ")
+
+#input_string=input("Enter the list with different Element :")
+#user_list  = input_string.split()
+#sum=0
+#for i in user_list:
+   # sum += int (i)
+#print('List :',user_list,'\n','Sum of the List= :',sum)
+
+
+#Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+
+#Books = ('Python with Django', 'Darts with Flutter', 'Website Dev with Java Script', 'Cisco with CCNA','Database Managemnt sytstem')
+#print('-------------------------------------')
+#print('List of my Favorite Books')
+#print('-------------------------------------')
+#for i in range(5):
+ #   print(Books[i])
+
+#Write a program that uses a dictionary to store information about a person, such as their name, age, and favorite color. Ask the user for input and store the information in the dictionary. Then, print the dictionary to the console.
+
+
+
+#Write a program that accepts user input to create two sets of integers. Then, create a new set that contains only the elements that are common to both sets.
+
+
+#Create a program that stores a list of words. Then, use list comprehension to create a new list that contains only the words that have an odd number of characters.
+
